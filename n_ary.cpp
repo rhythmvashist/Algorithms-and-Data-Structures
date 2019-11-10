3aab6b7fe7d45186fa26b016c05f64e34b505416
Rhythmvashist <vashistrhythm1@gmail.com>
2019-11-10 00:31:42 -0800

    improved the runtime

--- n_ary.cpp
+++ n_ary.cpp
@@ -1,6 +1,7 @@
 // Given an n-ary tree, return the preorder traversal of its nodes' values.
 #include <iostream>
 #include <vector>
+#include <stack>
 // Definition for a Node.
 class Node {
 public:
@@ -18,24 +19,42 @@ public:
 class Solution {
 public:
     
-    vector<int> preorder(Node* root) {
-        vector<int> res;
-        prorder(root,res);
-        return res;
-    }
+    // vector<int> preorder(Node* root) {
+    //     vector<int> res;
+    //     prorder(root,res);
+    //     return res;
+    // }
 
-    void prorder(Node*root ,vector<int> &rs){
-        if (root ==nullptr) 
-            return ;
+    // void prorder(Node*root ,vector<int> &rs){
+    //     if (root ==nullptr) 
+    //         return ;
         
-        rs.push_back(root->val);
-        for(auto child: root->children){
-            if (child!=nullptr){
-                prorder(child,rs);   
-            }
+    //     rs.push_back(root->val);
+    //     for(auto child: root->children){
+    //         if (child!=nullptr){
+    //             prorder(child,rs);   
+    //         }
                 
+    //     }
+    //     return;
+    // }
+  
+      vector<int> preorder(Node* root) {
+        if(root==nullptr) 
+            return {};
+        stack<Node*> st;
+        vector<int> ans;
+        st.push(root);
+        
+        while(!st.empty()){
+            Node* node = st.top();
+            st.pop();
+            ans.push_back(node->val);
+            for(int i=(node->children).size()-1;i>=0;--i){
+                st.push(node->children[i]);
+            }
         }
-        return;
+        return ans;
     }
 };
 
