49866dc320adf07a8d1d5fb62fdc07a412edf47b
Rhythmvashist <vashistrhythm1@gmail.com>
2019-11-13 09:01:44 -0800

    dub step loop sol

--- Dubstep.py
+++ Dubstep.py
@@ -9,8 +9,22 @@
 # WE ARE THE CHAMPIONS MY FRIEND 
 
 #one linear solution 
+# def dub_step(str):
+#   out = str.replace('WUB'," ")
+#   print(out)
+
 def dub_step(str):
-  out = str.replace('WUB'," ")
+  out=''
+  j=0
+  for i in range(0,len(str)):
+    if str[j]=='W' and str[j+1]=='U' and str[j+2]=='B':
+      j=j+2
+      out+=' '
+    else:
+      j=j+1
+
+      out+=str[j]
+
   print(out)
 
      
