d17e2e9d71b8c3eb13e77cdfddfd9df949a4c49f
Rhythmvashist <vashistrhythm1@gmail.com>
2019-11-12 21:28:58 -0800

    dub step one liner sol

--- Dubstep.py
+++ Dubstep.py
@@ -8,5 +8,22 @@
 # output
 # WE ARE THE CHAMPIONS MY FRIEND 
 
+#one linear solution 
 def dub_step(str):
+  out = str.replace('WUB'," ")
+  print(out)
+
+     
+
+        
+
+
   
+
+
+
+
+
+
+
+dub_step('WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB')
