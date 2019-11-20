8755f26f12b9034656d78cf3e17d5ed18ea38b29
Rhythmvashist <vashistrhythm1@gmail.com>
2019-11-19 23:44:36 -0800

    addded climibing the leaderborad

--- climbing_the_leaderboard.py
+++ climbing_the_leaderboard.py
@@ -8,10 +8,15 @@ import sys
 
 # Complete the climbingLeaderboard function below.
 def climbingLeaderboard(scores, alice):
-  print("done")
-  print(scores)
-  for i in range(1,scores):
-    print (i)
+    scores = sorted(list(set(scores)))
+    index = 0
+    rank_list = []
+    n = len(scores)
+    for i in alice:
+        while (n > index and i >= scores[index]):
+            index += 1
+        rank_list.append(n+1-index) 
+    return rank_list
 
 
 
