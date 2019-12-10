962d18c3f112c9f9c18efffc4f7c976fe236ab40
Rhythmvashist <vashistrhythm1@gmail.com>
2019-12-09 22:25:04 -0800

    improved time complexity of major elem O(n*n)) to O(n)

--- Major_Element.cpp
+++ Major_Element.cpp
@@ -7,27 +7,21 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
       int size = nums.size();
-      int count = 0;
-      int maxcount = 0;
-      int index =-1;
+      int count = 1;
+      int index =0;
       for (int i=0;i<size;i++){ 
-        for(int j=0;j<size;j++){   
-         
-          if (nums[i]==nums[j]){
-            count++;
-          }
-        
+        if (nums[index] == nums[i]){
+          count++;
         }
-        if (count >maxcount){
-          maxcount =count;
+        else{
+          count --;
+        }
+        if(count ==0){
           index = i;
+          count =1;
         }
-      }
-
-      if (maxcount > size/2){
-        return nums[index];
-      }
-
         
+
     }
+    return nums[index];
 }
