28b7975dabad94b86458ddcc1c31304b9f2533ed
Rhythmvashist <vashistrhythm1@gmail.com>
2019-12-09 22:19:35 -0800

    majority element

--- Major_Element.cpp
+++ Major_Element.cpp
@@ -7,17 +7,27 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
       int size = nums.size();
-      int elem = nums[0];
       int count = 0;
-      for (int i=0;i<size;i++){     
-        if (nums[i]== elem){
-          count++;
+      int maxcount = 0;
+      int index =-1;
+      for (int i=0;i<size;i++){ 
+        for(int j=0;j<size;j++){   
+         
+          if (nums[i]==nums[j]){
+            count++;
+          }
+        
         }
-        else{
-          count--;
+        if (count >maxcount){
+          maxcount =count;
+          index = i;
         }
-
       }
+
+      if (maxcount > size/2){
+        return nums[index];
+      }
+
         
     }
 }
