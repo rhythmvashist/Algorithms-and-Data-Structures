f4416859cdeab14138b9ef702d2aca7cf11d5583
Rhythmvashist <vashistrhythm1@gmail.com>
2019-12-11 23:44:35 -0800

    finished the sum_pair

--- sum_pair.cpp
+++ sum_pair.cpp
@@ -10,14 +10,34 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 using namespace std;
 
-bool pair_exist(vector<int> list ,int k){
+bool pair_exist(vector<int> A ,int sum){
     
-
+    int l, r; 
+    /* Sort the elements */
+    sort(A.begin(),A.end());
+    /* Now look for the two candidates in  
+       the sorted array*/
+    l = 0; 
+    r = A.size()-1; 
+    while (l < r) {   
+        if (A[l] + A[r] == sum) 
+            return true; 
+        else if (A[l] + A[r] < sum) 
+            l++; 
+        else // A[i] + A[j] > sum 
+            r--; 
+    } 
+    return false; 
 }
 
 int main(){
-  vector <int> list {10,15,3,7};
+  vector <int> list {10,15,3,55};
   int k = 17;
-  sort(list.begin(),list.end());
+  if(pair_exist(list,k)){
+    cout<<"yes"<<endl;
+  }
+  else{
+    cout<<"no"<<endl;
+  }
   return 0;
 }
