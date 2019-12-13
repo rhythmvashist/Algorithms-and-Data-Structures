f7e92b193ba1238d2e43f1e9265ac01bfb3b436c
Rhythmvashist <vashistrhythm1@gmail.com>
2019-12-13 00:23:35 -0800

    git added solution

--- number_of_island.cpp
+++ number_of_island.cpp
@@ -26,14 +26,30 @@ using namespace std;
 
 class Solution {
 public:
+    void bfstraversal(vector<vector<char>> grid, int i ,int j){
+      if(i<0 ||j<0|| j>=grid[i].size()||i>grid.size()||grid[i][j]=='0'){
+        return ;
+      }
+      grid[i][j]= '0';
+      bfstraversal(grid,i+1,j);
+      bfstraversal(grid,i,j+1);
+      bfstraversal(grid,i-1,j);
+      bfstraversal(grid,i,j-1);
+
+    }
+
     int numIslands(vector<vector<char>>& grid) {
+      int count=0;
       for(int i=0;i<grid.size();i++){
         for(int j=0;grid[i].size();j++){
+          if(grid[i][j]=='1'){
+            count++;
+            bfstraversal(grid,i,j);
+          }
           
         }
       }
+      return count ;
         
     }
-
-    
 };
