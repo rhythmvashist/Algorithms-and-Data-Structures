606967c101c32c92f5d385e613923fe5bd92652d
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-02-02 20:37:01 -0800

    made some changes

--- sum_contagious_subarray.cpp
+++ sum_contagious_subarray.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int get_sum_contagious_subarraay(vector<int> params){
-    for (int i = 0; i < params; i++)
-    {
-        /* code */
+    for (int i = 0; i < params.size; i++){
+        cout<<params[i]<<endl;
     }
     
 
-    
     return 0;
 }
 
 int main(){
+    vector<int> size {1,2,4,4};
+    int value = get_sum_contagious_subarraay(size);
     return 0;
 }
