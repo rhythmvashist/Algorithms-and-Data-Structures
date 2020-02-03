7d0ae0f00b7f457045bfcdfd8e782e816b815230
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-02-02 22:29:42 -0800

    fixed the issues in kadene algorithm with dp solution

--- sum_contagious_subarray.cpp
+++ sum_contagious_subarray.cpp
@@ -3,16 +3,35 @@
 using namespace std;
 
 int get_sum_contagious_subarraay(vector<int> params){
-    for (int i = 0; i < params.size; i++){
-        cout<<params[i]<<endl;
-    }
-    
+    int maximum_so_far = 0;
+    int maximum_till_here = 0;
 
-    return 0;
+    for(int i =0; i < params.size();i++){
+        
+        maximum_till_here = maximum_till_here + params[i] ;
+        if(maximum_till_here < 0){
+          maximum_till_here = 0;
+        }
+        if (maximum_till_here >maximum_so_far){
+            maximum_so_far = maximum_till_here; 
+        }
+
+    }
+    return maximum_so_far;
 }
 
 int main(){
-    vector<int> size {1,2,4,4};
-    int value = get_sum_contagious_subarraay(size);
+    vector<int> vec ;
+    // {-2, -3, 4, -1, -2, 1, 5, -3}; 
+    vec.push_back(-2);
+    vec.push_back(-3);
+    vec.push_back(4);
+    vec.push_back(-1);
+    vec.push_back(-2);
+    vec.push_back(1);
+    vec.push_back(5);
+    vec.push_back(-3);
+    int value = get_sum_contagious_subarraay(vec);
+    cout<<value<<endl;
     return 0;
 }
