4be038bdf5083de276c92c2b44412fa0622574b8
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-02-04 20:27:17 -0800

    working on color_problem

---
+++ color_problem.cpp
@@ -0,0 +1,19 @@
+//Given an undirected graph represented as an adjacency matrix and an integer k, write a function to determine
+// whether each vertex in the graph can be colored such that no two adjacent vertices share the same color using at most k colors.
+
+#include <iostream>
+#include <algorithm>
+
+# define V 4
+using namespace std;
+
+void graphSolution(bool graph[V][V],int k){
+
+}
+int main(){
+    bool graph[V][V] = {{0, 1, 1, 1}, {1, 0, 1, 0}, {1, 1, 0, 1}, {1, 0, 1, 0}, }; 
+    int k = 3
+
+    return 0;
+}
+
