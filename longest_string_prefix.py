6f318acd8894d1e7fa54950f98b0c704fa02c439
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-02-17 17:26:58 -0800

    implemted the longest string prefix in python and cpp

--- longest_string_prefix.py
+++ longest_string_prefix.py
@@ -13,5 +13,41 @@
 # Explanation: There is no common prefix among the input strings.
 
 
-def longestCommonPrefix(self, strs: List[str]) -> str:
-    print(strs)
+# the cpp solution
+# class Solution {
+# public:
+#     string longestCommonPrefix(vector<string>& strs) {
+#         if (strs.size() == 0) return "" ;
+#         string result = "";
+#         for(int i=0;i<strs[0].length();i++){
+#             for(int j=1;j<strs.size();j++){
+#                 if(i>strs[j].length() || strs[0][i] != strs[j][i]){
+#                     return result;
+#                 }      
+#             }
+#             result+=strs[0][i];
+#         }
+#         return result;
+#     }
+# };
+
+def longestCommonPrefix(strs) :
+    result = ""
+    if len(strs) == 0: return result
+
+    for count,c in enumerate(strs[0]):
+        for strings in strs:
+            if count > len(strings) or c != strings[count]:
+                return result
+        
+        result+=c
+    return result
+
+def main():
+    string_list =["flower","flow","flight"]
+    result = longestCommonPrefix(string_list)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
