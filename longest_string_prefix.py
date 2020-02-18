a49353a5f6cbd1de88389afd5340e665267b9127
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-02-17 17:27:41 -0800

    code improvement

--- longest_string_prefix.py
+++ longest_string_prefix.py
@@ -12,25 +12,6 @@
 # Output: ""
 # Explanation: There is no common prefix among the input strings.
 
-
-# the cpp solution
-# class Solution {
-# public:
-#     string longestCommonPrefix(vector<string>& strs) {
-#         if (strs.size() == 0) return "" ;
-#         string result = "";
-#         for(int i=0;i<strs[0].length();i++){
-#             for(int j=1;j<strs.size();j++){
-#                 if(i>strs[j].length() || strs[0][i] != strs[j][i]){
-#                     return result;
-#                 }      
-#             }
-#             result+=strs[0][i];
-#         }
-#         return result;
-#     }
-# };
-
 def longestCommonPrefix(strs) :
     result = ""
     if len(strs) == 0: return result
