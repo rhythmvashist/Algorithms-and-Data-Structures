ddf6ec748f7733b56db6dcf82972768a1fa1efcd
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-02-19 09:18:16 -0800

    working om remove duplicates using sets

---
+++ removeDulpticate.cpp
@@ -0,0 +1,43 @@
+// Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Given nums = [1,1,2],
+
+// Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+// It doesn't matter what you leave beyond the returned length.
+
+#include <iostream>
+#include <vector> 
+#include <set>
+
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+            set<int> result;
+            for(int i=0;i<nums.size();i++){
+                int x=nums[i];
+                cout<<x<<endl;
+                // that means element is not present 
+                if(result.find(x)==result.end() ){
+                    
+                    result.insert(x);
+                    cout<<"inserting :"<<x<<endl;
+                }   
+            }
+            return result.size();
+        
+}
+
+int main(){
+    vector<int> input ;
+    input.push_back(1);
+    input.push_back(1);
+    input.push_back(2);
+    int result = removeDuplicates(input);
+    cout<<result<<endl;
+    return 0;
+}
