2d566cd55e6e9fa681c3551827e73e13172f22fb
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-04-04 14:45:40 -0700

    Added leetcode directory

---
+++ Leetcode_30_day/singlenum.cpp
@@ -0,0 +1,59 @@
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+// Example 1:
+// Input: [2,2,1]
+// Output: 1
+
+// Example 2:
+// Input: [4,1,2,1,2]
+// Output: 4
+
+
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+int singleNumber(int nums[],int n) 
+{ 
+    map<int,int> m; 
+    long sum1 = 0,sum2 = 0; 
+  
+    for(int i = 0; i < n; i++) { 
+        if(m[nums[i]] == 0) { 
+            sum1 += nums[i]; 
+            m[nums[i]]++; 
+        } 
+        sum2 += nums[i]; 
+    } 
+      
+    // applying the formula. 
+    return 2 * (sum1) - sum2; 
+} 
+
+int getSingleNumber(int nums[],int n){
+  map<int,int> m;
+
+  for(int i=0;i<n;i++){
+    m[nums[i]]++;
+  }
+  for(int i=0;i<n;++i){
+    if(m[nums[i]]==1){
+      return nums[i];
+    }
+  }
+  return 0;
+}
+  
+// Driver code 
+int main() 
+{ 
+    int a[] = {2, 3, 5, 4, 5, 3, 4}; 
+    int n = 7; 
+    cout << singleNumber(a,n) << "\n"; 
+  
+    int b[] = {15, 18, 16, 18, 16, 15, 89}; 
+  
+    cout << getSingleNumber(b,n)<<"\n"; 
+    return 0; 
+} 
