4398fd857f7802d3fbc90ce7bab58f4c2e54fc86
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-04-10 18:59:58 -0700

    added group anagram solution :

---
+++ Leetcode_30_day/group_anagrams.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <map>
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int size = 0;
+        map<string,vector<string>> result;
+        for(string s :strs){
+            string s2 = s;
+            sort(s2.begin(),s2.end());
+            result[s2].push_back(s);
+        }
+        vector<vector<string>> out;
+        for (auto pp:result){
+            out.push_back(pp.second);
+        }
+        return out;
+    }
+};
