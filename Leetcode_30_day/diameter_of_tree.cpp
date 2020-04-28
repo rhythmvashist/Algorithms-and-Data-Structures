846f28bc756d1d7273b6e458344072559383c7e3
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-04-27 23:13:17 -0700

    started working on diameter on tree

--- Leetcode_30_day/diameter_of_tree.cpp
+++ Leetcode_30_day/diameter_of_tree.cpp
@@ -12,3 +12,19 @@
 // Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
 
 // Note: The length of path between two nodes is represented by the number of edges between them.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        
+    }
+};
