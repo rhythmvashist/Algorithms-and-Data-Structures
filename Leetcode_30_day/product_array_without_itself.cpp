94499be2e3b62528b441e4eea138039591d44853
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-05-09 01:27:07 -0700

    finished the product_array_without_itself

--- Leetcode_30_day/product_array_without_itself.cpp
+++ Leetcode_30_day/product_array_without_itself.cpp
@@ -13,3 +13,47 @@ Follow up:
 Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
 
 */
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<int> productExceptSelf(vector<int>& nums) {
+    int size = nums.size();
+    vector<int> ans(size) ;
+    int product =1 ;
+    int product_with_zero=1;
+    int zeroc=0;
+
+    for(int t:nums){
+        product*=t;
+        if(t==0){
+            zeroc++;
+        }
+        else{
+            product_with_zero*=t;
+            
+        }
+    }
+
+    if(product!=0){
+        for(int i=0;i<size;i++){
+            ans[i]=product/nums[i];
+        }
+    }
+    else if(zeroc==1){
+        for(int i=0;i<size;i++){
+            if(nums[i]==0){
+                ans[i]= product_with_zero;
+            }
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<int> input={1,2,3,4};
+    vector<int> result= productExceptSelf(input);
+}
