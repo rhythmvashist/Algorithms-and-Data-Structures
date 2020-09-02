d38a3498c0d43dc0f0c27433304622fc40eb1e0b
Rhythm Vashist <vashistrhythm1@gmail.com>
2020-09-02 03:49:56 -0700

    finished the problem

--- largest_time_digit.cpp
+++ largest_time_digit.cpp
@@ -5,7 +5,6 @@
 
 // Return the answer as a string of length 5.  If no valid time can be made, return an empty string.
 
-
 // Example 1:
 
 // Input: [1,2,3,4]
@@ -15,15 +14,79 @@
 // Input: [5,5,5,5]
 // Output: ""
 
-
-
 #include <iostream>
+#include <string>
 #include <vector>
-
 using namespace std;
 
-int main(){
-    cout<<"I'm back cpp let rock n roll";
-    vector<int> name={1,2};
+void getPermutation(string str, int low, int high, vector<string> &res)
+{
+    if (low == high)
+    {
+        res.push_back(str);
+    }
+    else
+    {
+        for (int i = low; i <= high; i++)
+        {
+            swap(str[low], str[i]);
+            getPermutation(str, low + 1, high, res);
+            swap(str[low], str[i]);
+        }
+    }
+}
 
+//checking if the given string is valid time
+bool isValid(string str)
+{
+    if ((stoi(str.substr(0, 2))) <= 23 && 60 >= stoi(str.substr(2, str.length())))
+    {
+        return true;
+    }
+    return false;
+}
+
+string largestTimeFromDigits(vector<int> &A)
+{
+    string result = "";
+
+    string str = "";
+    for (int val : A)
+    {
+        str += to_string(val);
+    }
+    vector<string> res;
+    getPermutation(str, 0, str.length() - 1, res);
+
+    for (int i = 0; i < res.size(); i++)
+    {
+        if (isValid(res[i]))
+        {
+            if (result == "")
+            {
+                result = res[i];
+            }
+            else
+            {
+                if (stoi(res[i]) > stoi(result))
+                {
+                    result = res[i];
+                }
+            }
+        }
+    }
+
+    string out = "";
+    for (int i = 0; i < result.length(); i++){
+        if (i == 2){
+            out += ":";
+        }
+        out += result[i];
+    }
+    return out;
+}
+
+int main() {
+  vector<int> a={2,2,2,2};
+  cout<<largestTimeFromDigits(a);
 }
